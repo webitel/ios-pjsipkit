807c3e0cc27786b3cc0ac31a548b5ff51b076a9d
Yurii Zhuk <iamyuriizr@gmail.com>
2025-08-21 20:58:58 +0300

    Add pj_wrapper file

---
+++ Sources/PJSIPKit/pj_wrapper.c.c
@@ -0,0 +1,8 @@
+//
+//  pj_wrapper.c.c
+//  VoiceSDK
+//
+//  Created by Yurii Zhuk on 20.06.2025.
+//
+
+#include "pj_wrapper.c.h"
---
+++ Sources/PJSIPKit/pj_wrapper.c.h
@@ -0,0 +1,13 @@
+//
+//  pj_wrapper.c.h
+//  VoiceSDK
+//
+//  Created by Yurii Zhuk on 20.06.2025.
+//
+
+#ifndef pj_wrapper_c_h
+#define pj_wrapper_c_h
+
+#include <stdio.h>
+
+#endif /* pj_wrapper_c_h */
