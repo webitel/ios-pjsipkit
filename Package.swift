cc54069d4dc3cdab583241868072f0bdd5772430
Yurii Zhuk <iamyuriizr@gmail.com>
2025-11-05 15:12:03 +0200

    Rebuilt PJSIP with video support enabled

--- Package.swift
+++ Package.swift
@@ -18,6 +18,10 @@ let package = Package(
             linkerSettings: [
                 .linkedFramework("AudioToolbox"),
                 .linkedFramework("AVFoundation"),
+                .linkedFramework("VideoToolbox"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo"),
+                .linkedFramework("QuartzCore"),
                 .linkedFramework("Network"),
             ]
         ),
